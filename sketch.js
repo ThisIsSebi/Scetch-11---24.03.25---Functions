1f4579c5f9c1e54955d699e880b84e55a9b86bf7
ThisIsSebi <shoff439@gmail.com>
2025-03-25 09:52:50 +0100

    Tried to implement Stop and continue Ball / Stop Ball works

--- sketch.js
+++ sketch.js
@@ -1,45 +1,59 @@
 var ball = {
-  x: 300, 
-  y: 200,
+  x: 300,
+  y: 400,
   xspeed: 4,
-  yspeed: -3
+  yspeed: -3,
+};
+
+function setup() {
+  createCanvas(600, 400);
 }
 
-function setup(){
-  createCanvas(600,400);
-}
-
-function draw(){
+function draw() {
   background(0);
 
-  displayBall()
+  displayBall();
 
-  bounceBall()
+  bounceBall();
 
-  moveBall()
+  moveBall();
 
-  stopBall()
+  stopBall();
+
 }
 
-function displayBall(){
-  stroke(255,0,0);
+function displayBall() {
+  stroke(255, 0, 0);
   strokeWeight(10);
   noFill();
   ellipse(ball.x, ball.y, 50, 50);
 }
 
-function bounceBall(){
-  if(ball.x > width || ball.x < 0){
+function bounceBall() {
+  if (ball.x > width || ball.x < 0) {
     ball.xspeed = ball.xspeed * -1;
   }
-  if(ball.y > height || ball.y < 0){
-    ball.yspeed = ball.yspeed *-1;
+  if (ball.y > height || ball.y < 0) {
+    ball.yspeed = ball.yspeed * -1;
   }
 }
 
-function moveBall(){
-  ball.x = ball.x + ball.xspeed;
-  ball.y = ball.y + ball.yspeed;
-
+function moveBall() {
+    ball.x = ball.x + ball.xspeed;
+    ball.y = ball.y + ball.yspeed;
+  
 }
 
+function stopBall() {
+  if (mouseIsPressed) {
+    ball.xspeed = 0;
+    ball.yspeed = 0;
+  }
+}
+
+function continueBall(){
+  if(key === 'a'){
+    ball.xspeed = 4;
+    ball.yspeed = -3;
+  }
+}
